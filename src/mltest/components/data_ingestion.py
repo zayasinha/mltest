1efc475dbe6922a953c56ea10dc763666bc81ce1
Jaya Sinha <sinhajaya601@gmail.com>
2025-11-11 18:48:50 +0530

    Data ingestion update

--- src/mltest/components/data_ingestion.py
+++ src/mltest/components/data_ingestion.py
@@ -28,8 +28,8 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=54)
-
-            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+           
             df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
             logging.info("Ingestion of the data is completed")
             return (
